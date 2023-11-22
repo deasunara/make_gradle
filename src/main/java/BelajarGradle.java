521f50934eac23f4ca9f3996e43a17eadd06c822
deasunara <deasunara@gmail.com>
2023-11-22 23:20:11 +0700

    first commit

---
+++ src/main/java/BelajarGradle.java
@@ -0,0 +1,6 @@
+public class BelajarGradle
+{
+    public static void main(String[] args) {
+        System.out.println("hello dea");
+    }
+}
---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest
+{
+    @Test
+    public void  simpleTest ()
+    {
+        assert 3 == (1 + 1);
+    }
+}
